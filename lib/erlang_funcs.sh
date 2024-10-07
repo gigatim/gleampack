1b3030c3dd245cdd05fdba13342af7147950cce0
Tim <tim@gigalixir.com>
2024-10-07 16:31:55 +0300

    fix: reference to erlang builds url

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -4,7 +4,7 @@ function erlang_tarball() {
 
 function download_erlang() {
   mkdir -p $(erlang_cache_path)
-  erlang_package_url="$(erlang_builds_url)/$(erlang_tarball)"
+  erlang_package_url="${erlang_builds_url}/$(erlang_tarball)"
 
   # If a previous download does not exist, then always re-download
   if [ ! -f $(erlang_cache_path)/$(erlang_tarball) ]; then
